f3b89d47d80f2af307083855a199ec1926ec58ac
Yuvasree_S <yuvasree0167@gmail.com>
2025-11-21 18:08:42 +0530

    Add files via upload

---
+++ Boolean_function.v
@@ -0,0 +1,12 @@
+module Boolean_function
+ (
+    input  wire A,
+    input  wire B,
+    input  wire C,
+    input  wire D,
+    output wire F
+);
+
+assign F = (~A & B) | (C & D) | (A & ~D);
+
+endmodule
